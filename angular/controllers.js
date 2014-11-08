d9480a72c8bd2d751c843825b24308682f338165
Christoffer <christofferskogh@hotmail.com>
2014-11-08 17:29:53 +0100

    Added angular menu
    The menu can be modified in the controller when we know what should be
    included in the menu.

---
+++ angular/controllers.js
@@ -0,0 +1,15 @@
+var onlinePlatform = angular.module('onlinePlatform', []);
+
+onlinePlatform.controller('onlinePlatformCtrl', function ($scope) {
+    
+    $scope.mainMenu = ['Hem', 'Nyheter', 'Meddelanden', 'Mina sidor', 'Forum', 'Kalender', 'Verktyg', 'Hjälp'],
+    $scope.subMenu = {
+        myPages: ['Mina Kurser', 'Mitt schema', 'Klasslista', 'Anteckningar']
+    }
+        
+    
+    
+    /*$scope.mainMenu = ['Hem', 'Meddelanden', 'Nyheter', 'Mina sidor', 'Hjälp', 'Verktyg', 'Forum'];
+    $scope.myPages = ['Mitt schema', 'Min klass', 'Mina kurser'];*/
+});
+
