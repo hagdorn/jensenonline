0533bcd83b1f29ae10e5fb39ad2269df5c968cb0
Christoffer <christofferskogh@hotmail.com>
2014-11-10 16:26:46 +0100

    First commit after controller rework

--- angular/controllers.js
+++ angular/controllers.js
@@ -2,12 +2,49 @@ var onlinePlatform = angular.module('onlinePlatform', []);
 
 onlinePlatform.controller('onlinePlatformCtrl', function ($scope) {
     
-    $scope.mainMenu = ['Startsida', 'Nyheter', 'Meddelanden', 'Mina sidor', 'Forum', 'Kalender', 'Verktyg', 'Hjälp'],
+    $scope.menus = [
+        
+        {
+            title: 'Startsida'    
+        },
+        {
+            title: 'Nyheter'    
+        },
+        {
+            title: 'Meddelanden',
+            items: ['Inkorg', 'Skickade meddelanden', 'Borttagna meddelande']
+        },
+        {
+            title: 'Mina sidor',
+            items: ['Mina kurser', 'Mitt schema', 'Klasslista', 'Anteckningar']
+        },
+        {
+            title: 'Forum'
+        },
+        {
+            title: 'Kalender'    
+        },
+        {
+            title: 'Verktyg',
+            items: ['Ladda ner Dreamspark produkter']
+        },
+        {
+            title: 'Hjälp'    
+        }
+        
+    ]
+    
+    
+    
+    
+    
+    
+    /*$scope.mainMenu = ['Startsida', 'Nyheter', 'Meddelanden', 'Mina sidor', 'Forum', 'Kalender', 'Verktyg', 'Hjälp'],
     $scope.subMenu = {
         messages: ['Inkorg', 'Skickat', 'Borttaget'],
         myPages: ['Mina kurser', 'Mitt schema', 'Klasslista', 'Anteckningar'],
         tools: ['Ladda ner Dreamspark produkter']
-    }
+    }*/
         
 });
 
