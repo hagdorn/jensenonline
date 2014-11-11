efaffb19abcaac37dbd7539f942dbce550e77aed
chrillewoodz <christofferskogh@hotmail.com>
2014-11-11 12:45:41 +0100

    Added login form

--- angular/controllers.js
+++ angular/controllers.js
@@ -79,18 +79,6 @@ onlinePlatform.controller('onlinePlatformCtrl', function ($scope) {
         }
         
     ]
-    
-    
-    
-    
-    
-    
-    /*$scope.mainMenu = ['Startsida', 'Nyheter', 'Meddelanden', 'Mina sidor', 'Forum', 'Kalender', 'Verktyg', 'Hjälp'],
-    $scope.subMenu = {
-        messages: ['Inkorg', 'Skickat', 'Borttaget'],
-        myPages: ['Mina kurser', 'Mitt schema', 'Klasslista', 'Anteckningar'],
-        tools: ['Ladda ner Dreamspark produkter']
-    }*/
-        
+
 });
 
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" data-ng-app="onlinePlatform" data-ng-controller="onlinePlatformCtrl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    
+    <title>Jensen Online Yrkeshögskola</title>
+</head>
+
+<body>
+
