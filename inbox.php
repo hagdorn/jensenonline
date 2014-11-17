6d88bd3f40b80257e0abc4966294153bf75aa0da
Christoffer <christofferskogh@hotmail.com>
2014-11-17 22:48:54 +0100

    Added checkbox + name

--- inbox.php
+++ inbox.php
@@ -25,37 +25,14 @@
         
         <div id="inbox">
             <ul id="first-ul">
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
-                <li><a href="#">Sed ut perspiciatis unde omnis iste natus error... <span><?php echo date('Y-m-d'); ?></span></a></li>
+                <li ng-repeat="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]">
+                    <form method="" action="">
+                        <input type="checkbox" id="checkbox{{i}}"><label for="checkbox{{i}}">Christoffer Skogh</label>
+                    </form>
+                    <a href="#">Sed ut perspiciatis unde omnis iste natus error... 
+                        <span><?php echo date('Y-m-d'); ?></span>
+                    </a>
+                </li>
             </ul>
             <footer>
                     <ul>
