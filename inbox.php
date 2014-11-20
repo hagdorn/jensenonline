c53b342c1e11ef8c2dbc11b4b7bc5cc6681b639d
Christian Hagdorn <hagdorn@gmail.com>
2014-11-20 13:15:35 +0100

    .messages

--- inbox.php
+++ inbox.php
@@ -15,14 +15,16 @@
         <?php include('includes/msgmenu.php'); ?>
         <div id="inbox">
             <ul id="first-ul">
-                <li ng-repeat="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]">
+                	<?php require_once('php/pm/getInbox.php'); ?>
+				<!--
+				<li ng-repeat="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]">
                     <form method="" action="">
                         <input type="checkbox" id="checkbox{{i}}"><label for="checkbox{{i}}">Christoffer Skogh</label>
                     </form>
                     <a href="#">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...
                         <span><?php echo date('Y-m-d'); ?></span>
                     </a>
-                </li>
+                </li>-->
             </ul>
             
             <footer>
---
+++ php/pm/getInbox.php
@@ -0,0 +1,35 @@
+<?php
+	try{
+		require_once('php/pdoConnect.php');
+		
+        $query = "SELECT * ";
+        $query .= "FROM pm ";
+        $query .= "WHERE touser = " .$_SESSION['id'];
+     
+		$result = $db->query($query);
+		
+		while ($message = $result->fetch()){
+			//echo "<br>".$row['message'];
+			echo("<li>");
+			echo("<form method='' action=''>  <input type='checkbox' id='" .$message['id'] ."'><label for='" .$message['id'] ."'>" .$message['fromuser'] ."</label></form>");
+			echo("<a href='#'>".$message['subject']);
+			echo("<span>" .$message['datetime'] ."</span></a></li>");
+		}
+	}
+	catch(Exception $exception){
+		echo('Error<br<br>' . $exception);
+	}
+
+
+
+?>
+
+	<!--
+				<li ng-repeat="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]">
+                    <form method="" action="">
+                        <input type="checkbox" id="checkbox{{i}}"><label for="checkbox{{i}}">Christoffer Skogh</label>
+                    </form>
+                    <a href="#">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...
+                        <span><?php echo date('Y-m-d'); ?></span>
+                    </a>
+                </li>-->
