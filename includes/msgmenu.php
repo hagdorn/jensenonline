415f4f5aeb9269ad1e064453d8fd56ddfb2927ba
chrillewoodz <christofferskogh@hotmail.com>
2014-11-20 12:10:48 +0100

    .

--- includes/msgmenu.php
+++ includes/msgmenu.php
@@ -1,11 +1,10 @@
 <div>
     <nav>
         <ul>
-            <li><a href="inbox.php">Olästa</a></li>
+            <li><a href="inbox.php">Inkorg</a></li>
             <li><a href="sendmsg.php">Skicka nytt</a></li>
-            <li><a href="#">Skickade</a></li>
-            <li><a href="#">Lästa</a></li>
-            <li><a href="#">Borttaget</a></li>
+            <li><a href="sent.php">Skickade</a></li>
+            <li><a href="removed.php">Borttaget</a></li>
         </ul>
     </nav>
 </div>
