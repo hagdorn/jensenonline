3e40131916bca7a8883dd2c30aaca94e0717723e
Christoffer <christofferskogh@hotmail.com>
2014-11-20 16:43:30 +0100

    -

--- inbox.php
+++ inbox.php
@@ -16,21 +16,10 @@
         <div id="inbox">
             <table>
                 	<?php require_once('php/pm/getInbox.php'); ?>
-<<<<<<< HEAD
-				<!--
-				<li ng-repeat="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]">
-                    <form method="" action="">
-                        <input type="checkbox" id="checkbox{{i}}"><label for="checkbox{{i}}">Christoffer Skogh</label>
-                    </form>
-                    <a href="#">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium...
-                        <span><?php echo date('Y-m-d'); ?></span>
-                    </a>
-                </li>-->
+
             </table>
-=======
-            </ul>
->>>>>>> FETCH_HEAD
-            
+
+
             <footer>
                     <ul>
                         <a href="#"><li id="first-li">Sida 1</li></a>
