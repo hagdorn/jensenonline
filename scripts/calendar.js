2aeaf9405a275dd51f80b847158b0bc9c7c69d5a
Christoffer <christofferskogh@hotmail.com>
2014-11-25 17:24:10 +0100

    new moder fader menu bug..

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -14,7 +14,7 @@ var container = document.getElementById('calendar-container');
 
 var dataModel = {
 
-    months: ['January', 'February', 'Marsch', 'April', 'May', 'June',
+    months: ['January', 'February', 'March', 'April', 'May', 'June',
              'July', 'August', 'September', 'October', 'November', 'December'
             ],
     daysInMonth: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31], 
