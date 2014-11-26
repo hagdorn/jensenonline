ab575167f756f1917c66b38fd63b20d1a9369eeb
Christoffer <christofferskogh@hotmail.com>
2014-11-26 21:38:38 +0100

    Calender bugfix, button bugfix to go

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -115,15 +115,15 @@ var calendarView = {
                     var selectYear = document.getElementById('year');
                     
                     function createOptions(category, element) {
-                        
+
                         for (i = 0; i < calendarModel[category].length; i++) {
                             var option = document.createElement('option');
-                                option.innerHTML = calendarModel[category[i]];
+                                option.innerHTML = calendarModel[category][i];
                                 option.value = option.innerHTML;
                                 element.appendChild(option);
                         }
                     }
-                    
+
                     createOptions('years', selectYear);
                     createOptions('months', selectMonth);
                 })(),
@@ -132,23 +132,11 @@ var calendarView = {
 var calendarController = {
     
     init: 
-        function() {
-            //Returns an array if the input was correct
-            var eventInfo = calendarController.validateInput();
-            
-            /*var newRef = calendarModel.fbRef.child(eventInfo[4]);
-                newRef.set({
-                    eventType: eventInfo[0], 
-                    eventTime: eventInfo[1], 
-                    eventLocation: eventInfo[2], 
-                    eventAddress: eventInfo[3], 
-                    eventDay: eventInfo[4]
-                });*/
-            
-            calendarView.createCalendar(0);
-            calendarModel.getRedDaysFromAPI(2015);
-            calendarController.addEventListnrs();
-        },
+                function() {
+                    calendarView.createCalendar(0);
+                    calendarModel.getRedDaysFromAPI(2015);
+                    calendarController.addEventListnrs();
+                },
     onChildAdded: 
                 calendarModel.fbRef.on('child_added', function(snapshot) { 
                     var eventInfo = snapshot.val();
@@ -180,20 +168,20 @@ var calendarController = {
                     }
                 },
     addEventListnrs: 
-                    function() {
-                        
-                        var addEventButton = document.getElementById('button'); 
-                            addEventButton.addEventListener('click', calendarController.validateInput);
-                        
-                        var deleteButton = document.getElementById('deleteButton');
-                            deleteButton.addEventListener('click', calendarController.confirmDelete);
+                function() {
 
-                        var showButton = document.getElementById('showButton');
-                            showButton.addEventListener('click', calendarController.scrollToMonth);
+                    var addEventButton = document.getElementById('button'); 
+                        addEventButton.addEventListener('click', calendarController.validateInput);
 
-                        var backToTopButton = document.getElementById('btt');
-                            backToTopButton.addEventListener('click', calendarController.scrollToTop);
-                    },
+                    var deleteButton = document.getElementById('deleteButton');
+                        deleteButton.addEventListener('click', calendarController.confirmDelete);
+
+                    var showButton = document.getElementById('showButton');
+                        showButton.addEventListener('click', calendarController.scrollToMonth);
+
+                    var backToTopButton = document.getElementById('btt');
+                        backToTopButton.addEventListener('click', calendarController.scrollToTop);
+                },
     validateInput: 
                 function() {
                     
@@ -205,6 +193,15 @@ var calendarController = {
                                 calendarModel.currentParent.id
                     ];
                      
+                    var newRef = calendarModel.fbRef.child(eventInfo[4]);
+                        newRef.set({
+                            eventType: eventInfo[0], 
+                            eventTime: eventInfo[1], 
+                            eventLocation: eventInfo[2], 
+                            eventAddress: eventInfo[3], 
+                            eventDay: eventInfo[4]
+                        });
+                    
                     //Check if any input values are too long
                     for (i = 0; i < calendarModel.inputErrorSnippets.length; i++) {
 
@@ -223,115 +220,122 @@ var calendarController = {
                             + ' is too long.');
                             return;
                         }
-                        else {
-                            return userInput;
-                        }
+                        
+                        
                     } 
                 },
-    toggleAddWindow: function() {
+    toggleAddWindow: 
+                function() {
 
-        var parent = this.parentNode;
-        var popup = document.getElementById('pop-up');
-        var typeOfEvent = document.getElementById('event');
-        var timeOfEvent = document.getElementById('time');
-        var locationOfEvent = document.getElementById('location');   
-        var addressOfEvent = document.getElementById('address'); 
+                    var parent = this.parentNode;
+                    var popup = document.getElementById('pop-up');
+                    var typeOfEvent = document.getElementById('event');
+                    var timeOfEvent = document.getElementById('time');
+                    var locationOfEvent = document.getElementById('location');   
+                    var addressOfEvent = document.getElementById('address'); 
+
+                    //Set the inputs to be empty when we open popup window
+                    typeOfEvent.value = '';
+                    timeOfEvent.value = '';
+                    locationOfEvent.value = '';
+                    addressOfEvent.value = '';
+
+                    if (popup.style.visibility === 'visible') {
+                        popup.style.visibility = 'hidden';
+                    }
+                    else {
+                        popup.style.visibility = 'visible'; 
+                    }
+
+                    calendarModel.currentParent = parent; 
+                },
+    showAddedEvent: 
+                function(eventType, eventTime, eventLocation, eventAddress, dayOfEvent) {
+
+                    var eventInfo = [eventType, eventTime, eventLocation, eventAddress];
+                    var dayElement = document.getElementById(dayOfEvent); 
+                    var popup = document.getElementById('pop-up');
+                        popup.style.visibility = 'hidden'; 
+
+                    for (i = 0; i < 4; i++) {
+                        var p = document.createElement('p');
+                            p.innerHTML = eventInfo[i];
+                            dayElement.appendChild(p); 
+                    }
+                },
+    deleteAll: 
+                function() {
+
+                    //Remove all events from the page
+                    var p_list = document.getElementsByTagName('p');
+
+                    for (i = p_list.length-1; i>=0; i--) { 
+                        p_list[i].parentNode.removeChild(p_list[i]); 
+                    }
+
+                    calendarModel.fbRef.remove();
+                },
+    deleteSingle: 
+                function() {
+
+                    var parent = this.parentNode; 
+                    var parentId = parent.id; 
+
+                    //Remove the event from the specific td
+                    var p_list = parent.getElementsByTagName('p');
+
+                    for (i = p_list.length-1; i>=0; i--) { 
+                        p_list[i].parentNode.removeChild(p_list[i]);
+                    }
+
+                    //Create a new Firebase reference that matches the parent ID
+                    //And remove the data at this location
+                    var removeData = new Firebase('https://event-calender.firebaseio.com/events/' + parentId);
+                        removeData.remove(); 
+                },
+    confirmDelete: 
+                function() {
         
-        //Set the inputs to be empty when we open popup window
-        typeOfEvent.value = '';
-        timeOfEvent.value = '';
-        locationOfEvent.value = '';
-        addressOfEvent.value = '';
+                    var conf = confirm('You are about to delete all events, this cannot be undone. Are you sure you want to continue?');
 
-        if (popup.style.visibility === 'visible') {
-            popup.style.visibility = 'hidden';
-        }
-        else {
-            popup.style.visibility = 'visible'; 
-        }
+                    switch (conf) {
 
-        calendarModel.currentParent = parent; 
-    },
-    showAddedEvent: function(eventType, eventTime, eventLocation, eventAddress, dayOfEvent) {
+                        case true: calendarController.deleteAll();
+                                   break;
 
-        var eventInfo = [eventType, eventTime, eventLocation, eventAddress];
-        var dayElement = document.getElementById(dayOfEvent); 
-        var popup = document.getElementById('pop-up');
-            popup.style.visibility = 'hidden'; 
-
-        for (i = 0; i < 4; i++) {
-            var p = document.createElement('p');
-                p.innerHTML = eventInfo[i];
-                dayElement.appendChild(p); 
-        }
-    },
-    deleteAll: function() {
-
-        //Remove all events from the page
-        var p_list = document.getElementsByTagName('p');
-
-        for (i = p_list.length-1; i>=0; i--) { 
-            p_list[i].parentNode.removeChild(p_list[i]); 
-        }
+                        case false: return; 
+                                    break;
+                    }
+                },
+    removeYearDiv: 
+                function() {
         
-        calendarModel.fbRef.remove();
-    },
-    deleteSingle: function() {
+                    while (calendarModel.siteContainer.firstChild) {
+                        calendarModel.siteContainer.removeChild(calendarModel.siteContainer.firstChild);
+                    }    
+                },
+    scrollToMonth: 
+                function() {
+        
+                    var y = document.getElementById('year');
+                    var selectedYear = y.options[y.selectedIndex].value;
+                    var m = document.getElementById('month'); 
+                    var selectedMonth = m.options[m.selectedIndex].value;
+                    var index = calendarModel.years.indexOf(selectedYear);
 
-        var parent = this.parentNode; 
-        var parentId = parent.id; 
-        
-        //Remove the event from the specific td
-        var p_list = parent.getElementsByTagName('p');
+                    calendarController.removeYearDiv();
 
-        for (i = p_list.length-1; i>=0; i--) { 
-            p_list[i].parentNode.removeChild(p_list[i]);
-        }
+                    calendarController.createCalendar(index);
+                    calendarModel.redDays(selectedYear);
 
-        //Create a new Firebase reference that matches the parent ID
-        //And remove the data at this location
-        var removeData = new Firebase('https://event-calender.firebaseio.com/events/' + parentId);
-            removeData.remove(); 
-    },
-    confirmDelete: function() {
-        
-        var conf = confirm('You are about to delete all events, this cannot be undone. Are you sure you want to continue?');
-
-        switch (conf) {
-
-            case true: calendarController.deleteAll();
-                       break;
-
-            case false: return; 
-                        break;
-        }
-    },
-    removeYearDiv: function() {
-        
-        while (calendarModel.siteContainer.firstChild) {
-            calendarModel.siteContainer.removeChild(calendarModel.siteContainer.firstChild);
-        }    
-    },
-    scrollToMonth: function() {
-        
-        var y = document.getElementById('year');
-        var selectedYear = y.options[y.selectedIndex].value;
-        var m = document.getElementById('month'); 
-        var selectedMonth = m.options[m.selectedIndex].value;
-        var index = calendarModel.years.indexOf(selectedYear);
-        
-        calendarController.removeYearDiv();
-        
-        calendarController.createCalendar(index);
-        calendarModel.redDays(selectedYear);
-        
-        $('html, body').animate({
-            scrollTop: $('#' + selectedYear + selectedMonth).offset().top
-        }, 'slow');
-    },
-    scrollToTop: function() {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-    }
+                    $('html, body').animate({
+                        scrollTop: $('#' + selectedYear + selectedMonth).offset().top
+                    }, 'slow');
+                },
+    scrollToTop: 
+                function() {
+                    $("html, body").animate({ scrollTop: 0 }, "slow");
+                }
 }
 
 window.onload = calendarController.init;
