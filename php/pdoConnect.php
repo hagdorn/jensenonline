02fd777615afa0d4870c3d23e5a4409aea105d10
chrillewoodz <christofferskogh@hotmail.com>
2014-12-09 09:46:44 +0100

    .

--- php/pdoConnect.php
+++ php/pdoConnect.php
@@ -1,10 +1,10 @@
 <?php
-try{
-	//$db = new PDO("mysql:dbname=120949-jensenonline;host=mysql14.citynetwork.se:3306", "120949-zj17807", "jensen2015");
-	$db = new PDO("mysql:dbname=jensenOnline;host=localhost", "root", "");
-}
-catch(Exception $exception){
-	echo("error <br><br>" . $exception);
-}
+    try{
+        //$db = new PDO("mysql:dbname=120949-jensenonline;host=mysql14.citynetwork.se:3306", "120949-zj17807", "jensen2015");
+        $db = new PDO("mysql:dbname=jensenOnline;host=localhost", "root", "");
+    }
+    catch(Exception $exception){
+        echo("error <br><br>" . $exception);
+    }
 ?>
 
