aa4e855a700338475791b0c7c51fac07086b6512
Christian Hagdorn <hagdorn@gmail.com>
2014-12-11 14:32:27 +0100

    msg

--- public/inbox.php
+++ public/inbox.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	require_once('../includes/functions/functions.php');
     include_header_files();
 
 	//Set the current box to be 'inbox'
@@ -12,7 +13,7 @@
     <?php include_once('../includes/msgtemplate.php'); ?>
         <div id="inbox-container">
             <table>
-                <?php require_once('../includes/php/pm/getInbox.php');?>
+                <?php include_once('../includes/php/pm/getInbox.php');?>
             </table>
         </div>
     </div> <!-- Closes the template include -->
