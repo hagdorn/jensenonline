4c83466d609d636575420b2f557b906619f1e5c7
Christoffer <christofferskogh@hotmail.com>
2014-12-15 21:55:16 +0100

    .

--- public/createsurvey.php
+++ public/createsurvey.php
@@ -4,12 +4,13 @@
     include_header_files();
 ?>
 
-
 <div id="survey-wrapper">
     <h1>Skapa enkät</h1>
     <h2>Välj hur enkäten ska se ut nedan.</h2>
     <select id="numOfQuestions"></select>
     <button id="addOneQuestion">Lägg till en ny fråga</button>
+    <button id="removeOneQuestion">Ta bort en fråga</button>
+    <span id="surveyErrorMsg"></span>
 </div>
 
 <?php
