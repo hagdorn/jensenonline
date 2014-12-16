191d3dff5a3323a97897c306b062035fbc309edf
Christoffer <christofferskogh@hotmail.com>
2014-12-17 00:01:59 +0100

    .

--- public/createsurvey.php
+++ public/createsurvey.php
@@ -10,8 +10,8 @@
         <h2>Välj hur enkäten ska se ut nedan.</h2>
         <select id="numOfQuestions"></select>
         <button id="addOneQuestion" class="survey-buttons green-button">Lägg till en ny fråga</button>
-        <span id="surveyErrorMsg"></span>
         <button id="lock-survey" class="survey-buttons orange-button">Lås enkäten</button>
+        <span id="surveyErrorMsg"></span>
         <div id="submit-container">
             <input type="submit" name="saveSurvey" form="survey-form" class="survey-buttons green-button">
             <input type="text" name="filename" form="survey-form">
